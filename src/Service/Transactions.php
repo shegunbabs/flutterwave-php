578990d63ce763a59eb1a7f0c3df84bda4f73c9a
shegun babs <shegun.babs@gmail.com>
2021-10-27 22:10:45 +0000

    create transaction service module

--- src/Service/Transactions.php
+++ src/Service/Transactions.php
@@ -4,7 +4,56 @@
 namespace Flutterwave\Service;
 
 
-class Transactions
+class Transactions extends AbstractService
 {
 
+    /**
+     * @param array $params = [
+     * 'from' => '...', 'to' => '...', 'page' => '...',
+     * 'customer_email' => '...', 'status' => '...', 'tx_ref' => '...',
+     *  'customer_fullname' => '...', 'currency' => '...',
+     * ]
+     * @return array
+     */
+    public function getAllTransactions(array $params) : array
+    {
+        $path = 'transactions';
+        return $this->get($path, $params);
+    }
+
+
+    public function getTransactionFee()
+    {
+
+    }
+
+
+    public function resendTransactionWebhook()
+    {
+
+    }
+
+
+    public function initiateTransactionRefund()
+    {
+
+    }
+
+
+    public function verifyTransaction()
+    {
+
+    }
+
+
+    public function viewTransactionTimeline()
+    {
+
+    }
+
+
+    public function getAllRefunds()
+    {
+
+    }
 }
